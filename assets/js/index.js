0eb024a3cb01822e30ee47498297882673718e0c
david.docampo <david.docampo@gmail.com>
2024-09-29 20:14:23 +0200

    comentarios en en index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,15 @@
+
+//capturamos la imagen con clase ".yoda" en la constante "imagen"
 const imagen = document.querySelector(".yoda")
 
-
+//le añadimos un cambio en caso de que el usuario haga click en la imagen
 imagen.addEventListener('click', function(){
+    //si tiene borde, se lo quitamos
     if(imagen.style.border === "2px solid red"){ 
        
         imagen.style.border = "none"
-    }else {
+    } // si no tiene borde, se lo añadimos
+    else {
         imagen.style.border = "2px solid red"
     }
 })
