109994a5ea692e20283b06f894cce434567f3ab4
david.docampo <david.docampo@gmail.com>
2024-09-29 20:24:25 +0200

    comentarios en el select.js

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,15 +1,16 @@
 
-
+//capturamos el botón que va a escuchar el click
 const btn = document.querySelector("#ingresar")
-
+//capturamos el párrafo que queremos cambiar
 let texto = document.querySelector("#texto")
-
+//al hacer click en el botón comprobamos el valor de los select
 btn.addEventListener("click", ()=>{
     const valor1 = document.querySelector("#valor1").value
     const valor2 = document.querySelector("#valor2").value
     const valor3 = document.querySelector("#valor3").value
+    //sumamos los select y nos dará un string
     const valores = valor1 + valor2 + valor3
-
+    //según el valor del string, cambiaremos el texto
     if(valores === '911'){
        return texto.textContent = "password 1 correcto" 
     } else if(valores === '714'){
